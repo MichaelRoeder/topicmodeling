576ad0ecfef9c51ab820a6ea6154b0189d81cbaa
Michael Röder <michael.roeder@uni-paderborn.de>
2023-05-08 11:59:15 +0200

    Added an export to Elasticsearch.

---
+++ topicmodeling.io.json/src/main/java/org/dice_research/topicmodeling/io/json/CorpusExporter4Elasticsearch.java
@@ -0,0 +1,117 @@
+package org.dice_research.topicmodeling.io.json;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import org.dice_research.topicmodeling.preprocessing.docconsumer.DocumentConsumer;
+import org.dice_research.topicmodeling.utils.doc.Document;
+import org.dice_research.topicmodeling.utils.doc.DocumentPropertyUtils;
+import org.dice_research.topicmodeling.utils.doc.StringContainingDocumentProperty;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.gson.stream.JsonWriter;
+
+/**
+ * This class writes JSON files into the given directory that can be used to
+ * load a corpus into Elasticsearch. Each file contains a single document. The
+ * properties of the document that should be written into the JSON file have to
+ * be registered with this object.
+ * 
+ * @author Michael R&ouml;der (michael.roeder@uni-paderborn.de)
+ *
+ */
+public class CorpusExporter4Elasticsearch implements DocumentConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CorpusExporter4Elasticsearch.class);
+
+    /**
+     * The directory to which the files will be written.
+     */
+    private File outputDirectory;
+    /**
+     * The function that will be used to derive the file name for a given document.
+     */
+    private Function<Document, String> documentNameGeneration = d -> Integer.toString(d.getDocumentId());
+    /**
+     * The registry that contains the keys and functions to derive the values based
+     * on the given document.
+     */
+    private Map<String, Function<Document, String>> registry = new HashMap<>();
+
+    /**
+     * Constructor.
+     * 
+     * @param outputDirectory The directory to which the files will be written
+     */
+    public CorpusExporter4Elasticsearch(File outputDirectory) {
+        this.outputDirectory = outputDirectory;
+    }
+
+    @Override
+    public void consumeDocument(Document document) {
+        String fileName = documentNameGeneration.apply(document);
+        if ((fileName == null) || fileName.isEmpty()) {
+            LOGGER.error("Got an empty file name for the given document. The document will be ignored.");
+            return;
+        }
+        String outputFile = outputDirectory.getAbsolutePath() + File.separator + fileName;
+        String value;
+        try (JsonWriter writer = new JsonWriter(new FileWriter(outputFile))) {
+            writer.beginObject();
+            for (String key : registry.keySet()) {
+                value = registry.get(key).apply(document);
+                if (value != null) {
+                    writer.name(key);
+                    writer.value(value);
+                }
+            }
+            writer.endObject();
+        } catch (IOException e) {
+            LOGGER.error("Exception while writing file \"" + outputFile + "\".", e);
+        }
+    }
+
+    /**
+     * Setter for the function that is used to derive the file name for a given
+     * document. By default, the document ID is used.
+     * 
+     * @param documentNameGeneration The function that will be used to derive the
+     *                               file name for a given document
+     */
+    public void setDocumentNameGeneration(Function<Document, String> documentNameGeneration) {
+        this.documentNameGeneration = documentNameGeneration;
+    }
+
+    /**
+     * Registers the given function with this consumer. For each document, the
+     * function will be called once to derive a value. The created key-value pair
+     * will be stored in the JSON of the document.
+     * 
+     * @param key      the key of the value that will be derived with the given
+     *                 function
+     * @param function the function that will be used to derive the value from the
+     *                 given document
+     */
+    public void registerKeyValuePair(String key, Function<Document, String> function) {
+        registry.put(key, function);
+    }
+
+    /**
+     * Registers the given StringContainingDocumentProperty class with this
+     * consumer. For each document, the value of the given property will be derived
+     * and stored with the given key in the document's JSON.
+     * 
+     * @param key   the key of the value that will be derived from the given
+     *              StringContainingDocumentProperty class
+     * @param clazz the StringContainingDocumentProperty class which will be used to
+     *              get the value for the given key
+     */
+    public void registerStringContainingProperty(String key, Class<? extends StringContainingDocumentProperty> clazz) {
+        registry.put(key, d -> DocumentPropertyUtils.getStringOrNull(d, clazz));
+    }
+}
